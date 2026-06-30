11074e3b8e2a41fce3d35e4588cd97c0c56e927f
Sourabh Joshi <sourabhjoshi2906@gmail.com>
2026-06-30 16:17:21 +0530

    minute change for deployment

--- backend/main.py
+++ backend/main.py
@@ -1128,4 +1128,6 @@ def supported_formats():
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host="localhost", port=8000, reload=True)
+    import os
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
